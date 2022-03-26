e34edb08fe593fc29bab631e053c99a7753ad21d
Yarolsav <netrodgen@rambler.ru>
2022-03-26 19:43:42 +0300

    feat: decomposition App

--- src/App.js
+++ src/App.js
@@ -4,32 +4,17 @@ import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts';
 
 import './styles/App.css'
 
 
 function App() {
   //состояние массива постов
-  const [posts, setPosts] = useState([
-    {id:1, title:'JavaScript', body:'Description'},
-    {id:2, title:'Python', body:'e'},
-    {id:3, title:'C++', body:'ya'},
-  ])
-
- const [filter, setFilter] = useState({sort:'', query:''})
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({sort:'', query:''})
   const [modal,setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    console.log('отработала функция сортед пост')
-    if(filter.sort){
-      return [...posts].sort((a,b)=> a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSeacrchedPosts = useMemo(()=>{
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  },[filter.query,sortedPosts])
+  const sortedAndSeacrchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost])
